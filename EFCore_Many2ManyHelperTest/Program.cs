e72ff55434d8a9fb2453003c5a259d3b810a90f3
Vladislav Shatravin <vladiksat@list.ru>
2019-03-19 19:12:52 +0700

    Add project files.

---
+++ EFCore_Many2ManyHelperTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EFCore_Many2ManyHelperTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
